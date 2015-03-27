9dab1b3aa7eba28e33740cac41c503b172d073b7
Celso Miranda <contacto@celsomiranda.net>
2015-03-27 01:34:53 +0000

    add markdown-it-anchor tests

--- test/index.js
+++ test/index.js
@@ -4,19 +4,21 @@ var util    = require('hexo-util');
 
 describe('Markdown-It Hexo Renderer', function(){
 
-  var render  = require('../lib/renderer')
+  var render  = require('../lib/renderer');
 
   it('Default (GFM)', function(){
     var ctx = {
       config: {
         markdown: {
-          html: false,
-          xhtmlOut: true,
-          breaks: true,
-          langPrefix: '',
-          linkify: true,
-          typographer: true,
-          quotes: '“”‘’'
+          render: {
+            html: false,
+            xhtmlOut: true,
+            breaks: true,
+            langPrefix: '',
+            linkify: true,
+            typographer: true,
+            quotes: '“”‘’'
+          },
         }
       }
     };
@@ -106,4 +108,51 @@ describe('Markdown-It Hexo Renderer', function(){
     r({text: 'H~2~O'} ).should.eql('<p>H<sub>2</sub>O</p>\n');
 
   });
+
+  it('Header Anchors', function(){
+    var ctx = {
+      config: {
+        markdown: {
+          anchors: {
+            level: 2,
+            permalink: false,
+            permalinkClass: 'header-anchor',
+            permalinkSymbol: '¶'
+          }
+        }
+      }
+    };
+
+    var r = render.bind(ctx);
+
+    var headers = [
+      '# Level 1 - Not anchored',
+      '',
+      '## Level 2',
+      '',
+      '## Level *2*',
+      '',
+      '## Level **2**',
+      '',
+      '### Level - $3',
+      '',
+      '#### Nível 4',
+      '',
+      '#### [Nível 4](http://www.celsomiranda.net)',
+      ''
+    ].join('\n');
+
+    r({text: headers}).should.eql([
+      '<h1>Level 1 - Not anchored</h1>',
+      '<h2 id="level-2">Level 2</h2>',
+      '<h2 id="level-2">Level <em>2</em></h2>',
+      '<h2 id="level-2">Level <strong>2</strong></h2>',
+      '<h3 id="level-3">Level - $3</h3>',
+      '<h4 id="nivel-4">Nível 4</h4>',
+      '<h4 id="nivel-4"><a href="http://www.celsomiranda.net">Nível 4</a></h4>',
+      ''
+    ].join('\n'));
+
+  });
+
 });
