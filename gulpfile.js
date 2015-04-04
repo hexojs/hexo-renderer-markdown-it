e5549f656488883c5a8b1174a0744809d0efe812
Celso Miranda <contacto@celsomiranda.net>
2015-04-05 00:36:14 +0100

    update tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,35 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var rirmaf = require('rimraf');
-
-var lib = 'lib/**/*.js';
+var del = require('del');
 
 gulp.task('coverage', function(){
-  return gulp.src(lib)
+  return gulp.src('lib/*.js')
     .pipe($.istanbul())
     .pipe($.istanbul.hookRequire());
 });
 
 gulp.task('coverage:clean', function(callback){
-  rirmaf('coverage', callback);
+  del(['coverage', 'string'], callback);
 });
 
 gulp.task('mocha', ['coverage'], function(){
   return gulp.src('test/index.js')
     .pipe($.mocha({
-      reporter: 'spec'
+      reporter: 'progress',
+      bail: true
     }))
     .pipe($.istanbul.writeReports());
 });
 
 gulp.task('jshint', function(){
-  return gulp.src(lib)
+  return gulp.src('lib/*.js')
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'))
     .pipe($.jshint.reporter('fail'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch(lib, ['mocha', 'jshint']);
+  gulp.watch('lib/*.js', ['mocha', 'jshint']);
   gulp.watch(['test/index.js'], ['mocha']);
 });
 
--- test/index.js
+++ test/index.js
@@ -7,91 +7,68 @@ var source  = fs.readFileSync('./test/fixtures/markdownit.md', 'utf8');
 
 describe('Hexo Renderer Markdown-it', function () {
 
-  it('should default to CommonMark if no config is provided', function () {
-
-    var parsed_without_config = fs.readFileSync('./test/fixtures/outputs/commonmark.html', 'utf8');
+  it('should render GFM if no config provided', function () {
+    var parsed_without_config = fs.readFileSync('./test/fixtures/outputs/default.html', 'utf8');
     var ctx = { config: { title: 'no markdown'}};
     var parse = render.bind(ctx);
     var result = parse({text: source});
-
     ctx = {};
-    result.should.eql(parsed_without_config);
-
+    result.should.equal(parsed_without_config);
   });
 
   it('should render CommonMark if config is \'commonmark\'', function () {
-
     var parsed_commonmark = fs.readFileSync('./test/fixtures/outputs/commonmark.html', 'utf8');
     var ctx = { config: { markdown: 'commonmark'}};
     var parse = render.bind(ctx);
     var result = parse({text: source});
-
     ctx = {};
-    result.should.eql(parsed_commonmark);
-
+    result.should.equal(parsed_commonmark);
   });
 
   it('should render a limited subset of Markdown if using \'zero\'', function () {
-
     var parsed_zero = fs.readFileSync('./test/fixtures/outputs/zero.html', 'utf8');
     var ctx = { config: { markdown: 'zero'}};
     var parse = render.bind(ctx);
     var result = parse({text: source});
-
     ctx = {};
-    result.should.eql(parsed_zero);
-
+    result.should.equal(parsed_zero);
   });
 
   it('should render something very close to GFM with \'default\'', function () {
-
     var parsed_gfm = fs.readFileSync('./test/fixtures/outputs/default.html', 'utf8');
     var ctx = { config: { markdown: 'default'}};
     var parse = render.bind(ctx);
     var result = parse({text: source});
-
     ctx = {};
-    result.should.eql(parsed_gfm);
-
+    result.should.equal(parsed_gfm);
   });
 
   it('should handle a custom configuration', function () {
-
     var parsed_custom = fs.readFileSync('./test/fixtures/outputs/custom.html', 'utf8');
     var ctx = { config: { markdown: { render: { html: false, xhtmlOut: true, breaks: true, langPrefix: '', linkify: true, typographer: true, quotes: '«»“”' }}}};
     var source  = fs.readFileSync('./test/fixtures/markdownit.md', 'utf8');
     var parse = render.bind(ctx);
     var result = parse({text: source});
-
     ctx = {};
-    result.should.eql(parsed_custom);
-
+    result.should.equal(parsed_custom);
   });
 
   it('should render plugins if they are defined', function () {
-
     var parsed_plugins = fs.readFileSync('./test/fixtures/outputs/plugins.html', 'utf8');
-    var ctx = { config: {markdown: { plugins:[ 'markdown-it-footnote', 'markdown-it-sub', 'markdown-it-sup' ]}}};
+    var ctx = { config: {markdown: { render: { html: false, xhtmlOut: false, breaks: false, langPrefix: 'language-', linkify: false, typographer: false, quotes: '«»“”' }, plugins:[ 'markdown-it-footnote', 'markdown-it-sub', 'markdown-it-sup', 'markdown-it-ins' ]}}};
     var source  = fs.readFileSync('./test/fixtures/markdownit.md', 'utf8');
     var parse = render.bind(ctx);
     var result = parse({text: source});
-
     ctx = {};
-    result.should.eql(parsed_plugins);
-
+    result.should.equal(parsed_plugins);
   });
 
   it('should render anchor-headers if they are defined', function () {
-
     var parsed_anchors = fs.readFileSync('./test/fixtures/outputs/anchors.html', 'utf8');
     var ctx = { config: { markdown: { anchors: { level: 2, permalink: false, permalinkClass: 'header-anchor', permalinkSymbol: '¶' }}}};
     var parse = render.bind(ctx);
     var result = parse({text: source});
-
     ctx = {};
-    result.should.eql(parsed_anchors);
-
+    result.should.equal(parsed_anchors);
   });
-
-
 });
