b43ef9f91ecb01a738152e5c16a4be560c8694fa
Celso Miranda <contacto@celsomiranda.net>
2015-04-04 08:51:33 +0100

    refactor the tests

--- test/index.js
+++ test/index.js
@@ -1,191 +1,97 @@
 'use strict';
+var fs      = require('fs');
+var render  = require('../lib/renderer');
+var chai    = require('chai');
+var should  = chai.should();
+var source  = fs.readFileSync('./test/fixtures/markdownit.md', 'utf8');
 
-var should  = require('chai').should();
+describe('Hexo Renderer Markdown-it', function () {
 
-describe('Markdown-It Hexo Renderer', function(){
+  it('should default to CommonMark if no config is provided', function () {
 
-  var render  = require('../lib/renderer');
+    var parsed_without_config = fs.readFileSync('./test/fixtures/outputs/commonmark.html', 'utf8');
+    var ctx = { config: { title: 'no markdown'}};
+    var parse = render.bind(ctx);
+    var result = parse({text: source});
 
-  it('Without Setup', function(){
-    var ctx = {
-      config:{
-        title: 'Something to test'
-      }
-    };
-
-    var r = render.bind(ctx);
-
-    var body = [
-      '# Hello',
-      'world',
-      '',
-      '| Option | Description |',
-      '| ------ | ----------- |',
-      '| data   | path to data files to supply the data that will be passed into templates. |',
-      '| engine | engine to be used for processing templates. Handlebars is the default. |',
-      '| ext    | extension to be used for dest files. |'
-    ].join('\n');
-
-    var result = r({text: body});
-
-    result.should.eql([
-        '<h1>Hello</h1>',
-        '<p>world</p>',
-        '<p>| Option | Description |',
-        '| ------ | ----------- |',
-        '| data   | path to data files to supply the data that will be passed into templates. |',
-        '| engine | engine to be used for processing templates. Handlebars is the default. |',
-        '| ext    | extension to be used for dest files. |</p>'
-      ].join('\n') + ('\n'));
-  });
-
-  it('Default (GFM)', function(){
-    var ctx = {
-      config: {
-        markdown: {
-          render: {
-            html: false,
-            xhtmlOut: true,
-            breaks: true,
-            langPrefix: '',
-            linkify: true,
-            typographer: true,
-            quotes: '“”‘’'
-          },
-        }
-      }
-    };
-
-    var r = render.bind(ctx);
-
-    var body = [
-      'Hello',
-      'world'
-    ].join('\n');
-
-    var result = r({text: body});
-
-    result.should.eql([
-        '<p>Hello<br />',
-        'world</p>'
-      ].join('\n') + ('\n'));
+    ctx = {};
+    result.should.eql(parsed_without_config);
 
   });
 
-  it('Strict CommonMark', function(){
-    var ctx = {
-      config: {
-        markdown: 'commonmark'
-      }
-    };
+  it('should render CommonMark if config is \'commonmark\'', function () {
 
-    var r = render.bind(ctx);
+    var parsed_commonmark = fs.readFileSync('./test/fixtures/outputs/commonmark.html', 'utf8');
+    var ctx = { config: { markdown: 'commonmark'}};
+    var parse = render.bind(ctx);
+    var result = parse({text: source});
 
-    var body = [
-      '# Hello',
-      'world',
-      '',
-      '| Option | Description |',
-      '| ------ | ----------- |',
-      '| data   | path to data files to supply the data that will be passed into templates. |',
-      '| engine | engine to be used for processing templates. Handlebars is the default. |',
-      '| ext    | extension to be used for dest files. |'
-    ].join('\n');
-
-    var result = r({text: body});
-
-    result.should.eql([
-        '<h1>Hello</h1>',
-        '<p>world</p>',
-        '<p>| Option | Description |',
-        '| ------ | ----------- |',
-        '| data   | path to data files to supply the data that will be passed into templates. |',
-        '| engine | engine to be used for processing templates. Handlebars is the default. |',
-        '| ext    | extension to be used for dest files. |</p>'
-      ].join('\n') + ('\n'));
+    ctx = {};
+    result.should.eql(parsed_commonmark);
 
   });
 
-  it('Plugins', function(){
-    var ctx = {
-      config: {
-        markdown: {
-          plugins: [
-            'markdown-it-footnote',
-            'markdown-it-sub',
-            'markdown-it-sup'
-          ]
-        }
-      }
-    };
-    var r = render.bind(ctx);
-    var footnote = [
-      'Test [^1]:',
-      '',
-      '[^1]: footnote'
-    ].join('\n');
+  it('should render a limited subset of Markdown if using \'zero\'', function () {
 
-    r({text: footnote}).should.eql([
-      '<p>Test <sup class="footnote-ref"><a href="#fn1" id="fnref1">[1]</a></sup>:</p>',
-      '<hr class="footnotes-sep">',
-      '<section class="footnotes">',
-      '<ol class="footnotes-list">',
-      '<li id="fn1"  class="footnote-item"><p>footnote <a href="#fnref1" class="footnote-backref">↩</a></p>',
-      '</li>',
-      '</ol>',
-      '</section>',
-      ''
-    ].join('\n'));
+    var parsed_zero = fs.readFileSync('./test/fixtures/outputs/zero.html', 'utf8');
+    var ctx = { config: { markdown: 'zero'}};
+    var parse = render.bind(ctx);
+    var result = parse({text: source});
 
-    r({text: '29^th^'}).should.eql('<p>29<sup>th</sup></p>\n');
-    r({text: 'H~2~O'} ).should.eql('<p>H<sub>2</sub>O</p>\n');
+    ctx = {};
+    result.should.eql(parsed_zero);
 
   });
 
-  it('Header Anchors', function(){
-    var ctx = {
-      config: {
-        markdown: {
-          anchors: {
-            level: 2,
-            permalink: false,
-            permalinkClass: 'header-anchor',
-            permalinkSymbol: '¶'
-          }
-        }
-      }
-    };
+  it('should render something very close to GFM with \'default\'', function () {
 
-    var r = render.bind(ctx);
+    var parsed_gfm = fs.readFileSync('./test/fixtures/outputs/default.html', 'utf8');
+    var ctx = { config: { markdown: 'default'}};
+    var parse = render.bind(ctx);
+    var result = parse({text: source});
 
-    var headers = [
-      '# Level 1 - Not anchored',
-      '',
-      '## Level 2',
-      '',
-      '## Level *2*',
-      '',
-      '## Level **2**',
-      '',
-      '### Level - $3',
-      '',
-      '#### Nível 4',
-      '',
-      '#### [Nível 4](http://www.celsomiranda.net)',
-      ''
-    ].join('\n');
-
-    r({text: headers}).should.eql([
-      '<h1>Level 1 - Not anchored</h1>',
-      '<h2 id="level-2">Level 2</h2>',
-      '<h2 id="level-2">Level <em>2</em></h2>',
-      '<h2 id="level-2">Level <strong>2</strong></h2>',
-      '<h3 id="level-3">Level - $3</h3>',
-      '<h4 id="nivel-4">Nível 4</h4>',
-      '<h4 id="nivel-4"><a href="http://www.celsomiranda.net">Nível 4</a></h4>',
-      ''
-    ].join('\n'));
+    ctx = {};
+    result.should.eql(parsed_gfm);
 
   });
+
+  it('should handle a custom configuration', function () {
+
+    var parsed_custom = fs.readFileSync('./test/fixtures/outputs/custom.html', 'utf8');
+    var ctx = { config: { markdown: { render: { html: false, xhtmlOut: true, breaks: true, langPrefix: '', linkify: true, typographer: true, quotes: '«»“”' }}}};
+    var source  = fs.readFileSync('./test/fixtures/markdownit.md', 'utf8');
+    var parse = render.bind(ctx);
+    var result = parse({text: source});
+
+    ctx = {};
+    result.should.eql(parsed_custom);
+
+  });
+
+  it('should render plugins if they are defined', function () {
+
+    var parsed_plugins = fs.readFileSync('./test/fixtures/outputs/plugins.html', 'utf8');
+    var ctx = { config: {markdown: { plugins:[ 'markdown-it-footnote', 'markdown-it-sub', 'markdown-it-sup' ]}}};
+    var source  = fs.readFileSync('./test/fixtures/markdownit.md', 'utf8');
+    var parse = render.bind(ctx);
+    var result = parse({text: source});
+
+    ctx = {};
+    result.should.eql(parsed_plugins);
+
+  });
+
+  it('should render anchor-headers if they are defined', function () {
+
+    var parsed_anchors = fs.readFileSync('./test/fixtures/outputs/anchors.html', 'utf8');
+    var ctx = { config: { markdown: { anchors: { level: 2, permalink: false, permalinkClass: 'header-anchor', permalinkSymbol: '¶' }}}};
+    var parse = render.bind(ctx);
+    var result = parse({text: source});
+
+    ctx = {};
+    result.should.eql(parsed_anchors);
+
+  });
+
 
 });
